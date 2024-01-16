80cea4d3ecb0a4b2d575240329bf697d9187cbf1
ohddang <ohddang509@gmail.com>
2024-01-17 00:39:50 +0900

    test : pull request test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
-import List from "./pages/List";
-import Post from "./pages/Post";
+import Home from "./pages/Home.jsx";
+import List from "./pages/List.jsx";
+import Post from "./pages/Post.jsx";
 
 function App() {
   return (
