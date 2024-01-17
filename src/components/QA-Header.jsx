94bc761f89601fb442e515aa454e50d59f6ca2e4
eundin <escience1202@gmail.com>
2024-01-17 19:42:24 +0900

    design: QA header 공유하기 버튼 디자인 완성 + svg파일 옵션 수정

---
+++ src/components/QA-Header.jsx
@@ -0,0 +1,60 @@
+import styled from "styled-components";
+import "../common.css";
+// 이미지 파일들
+import headerPng from "../assets/header.png";
+import logoSvg from "../assets/logo.svg";
+import personSvg from "../assets/Person.svg";
+import linkSvg from "../assets/Link.svg";
+import kakaotalkSvg from "../assets/Kakaotalk.svg";
+import facebookSvg from "../assets/Facebook.svg";
+
+const COLOR = {
+  link: "--Brown-40",
+  kakaotalk: "--Yellow-50",
+  facebook: "--Blue-50",
+};
+
+const ShareButton = styled.span`
+  background-color: var(${({ icon }) => COLOR[icon]});
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 40px;
+  height: 40px;
+`;
+
+const ShateButtonBox = styled.div`
+  gap: 12px;
+  display: flex;
+  width: 144px;
+  height: 40px;
+`;
+
+// 유저 데이터 받아와야함 - 잠시 아이콘으로 대체
+// 컴포넌트 스타일로 만들어야 할 것 - 유저 프로필, 공유하기 버튼, 공유하기 버튼 레이아웃, 유저명?, header큰 레이아웃,
+function QaHeader() {
+  return (
+    <header>
+      <img src={headerPng} />
+      <div>
+        <img src={logoSvg} />
+        <img src={personSvg} />
+        <span>유저명</span>
+        <ShateButtonBox>
+          <ShareButton icon="link">
+            <img src={linkSvg} width="18" height="18" />
+          </ShareButton>
+          <ShareButton icon="kakaotalk">
+            <img src={kakaotalkSvg} />
+          </ShareButton>
+          <ShareButton icon="facebook">
+            <img src={facebookSvg} />
+          </ShareButton>
+        </ShateButtonBox>
+      </div>
+    </header>
+  );
+}
+
+export default QaHeader;
--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,5 +1,13 @@
+// 테스트 하려고 Post에 잠깐 import해서 사용중
+import QaHeader from "components/QA-Header";
+
 function Post() {
-  return <div>Post</div>;
+  return (
+    <>
+      <div>Post</div>
+      <QaHeader />
+    </>
+  );
 }
 
 export default Post;
