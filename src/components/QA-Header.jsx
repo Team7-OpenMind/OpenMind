0cbc6dac410bd8bd882cd2ab2b6179d9cdf14397
Eun Din <escience1202@gmail.com>
2024-01-17 19:13:39 +0900

    design: 로고 스타일 컴포넌트로 만듬

--- src/components/QA-Header.jsx
+++ src/components/QA-Header.jsx
@@ -21,7 +21,7 @@ const ShareButton = styled.span`
   justify-content: center;
   align-items: center;
   width: 40px;
-  height: 40px;
+  height: 40p x;
 `;
 
 const ShareButtonBox = styled.div`
@@ -31,6 +31,11 @@ const ShareButtonBox = styled.div`
   height: 40px;
 `;
 
+const Logo = styled.img`
+  width: 124px;
+  height: 49px;
+`;
+
 // 유저 데이터 받아와야함 - 잠시 아이콘으로 대체
 // 컴포넌트 스타일로 만들어야 할 것 - 유저 프로필, 유저명?, header큰 레이아웃,
 function QaHeader() {
@@ -38,18 +43,18 @@ function QaHeader() {
     <header>
       <img src={headerPng} />
       <div>
-        <img src={logoSvg} />
+        <Logo src={logoSvg} alt="로고" />
         <img src={personSvg} />
         <span>유저명</span>
         <ShareButtonBox>
           <ShareButton icon="link">
-            <img src={linkSvg} />
+            <img src={linkSvg} alt="링크 아이콘" />
           </ShareButton>
           <ShareButton icon="kakaotalk">
-            <img src={kakaotalkSvg} />
+            <img src={kakaotalkSvg} alt="카카오톡 아이콘" />
           </ShareButton>
           <ShareButton icon="facebook">
-            <img src={facebookSvg} />
+            <img src={facebookSvg} alt="페이스북 아이콘" />
           </ShareButton>
         </ShareButtonBox>
       </div>
