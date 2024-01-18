16d4185e24ccd73b23a171c810464b0e5609ddbd
Jinah <monsatfri@gmail.com>
2024-01-18 15:19:38 +0900

    chore: react-query 사용하기 위한 컴포넌트 추가

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "App";
 import "common.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>,
 );
