c38cd568107579825f8c0ac99ce2342ee2d6f837
Jinah <monsatfri@gmail.com>
2024-01-23 14:32:15 +0900

    OM-14 feat: 질문 피드 미답변/답변 완료/답변 거절 상태 추가

--- src/components/feedCard/FeedCard.jsx
+++ src/components/feedCard/FeedCard.jsx
@@ -1,6 +1,12 @@
 import styled from "styled-components";
 import { getTimeAgo } from "utils/date";
 
+const colors = {
+  answered: "var(--Brown-40)",
+  rejected: "var(--Red-50)",
+  unanswered: "var(--Grayscale-40)",
+};
+
 const FeedCardStyled = styled.div`
   display: flex;
   flex-direction: column;
@@ -27,11 +33,9 @@ const AnsweredStyled = styled.div`
   padding: 4px 16px;
   width: fit-content;
   background: var(--Grayscale-10);
-  border: 1px solid
-    ${(props) => (props.answered ? "var(--Brown-40)" : "var(--Grayscale-40)")};
+  border: 1px solid ${(props) => colors[props.color]};
   border-radius: 8px;
-  color: ${(props) =>
-    props.answered ? "var(--Brown-40)" : "var(--Grayscale-40)"};
+  color: ${(props) => colors[props.color]};
 `;
 
 const QuestionContainer = styled.div`
@@ -78,6 +82,13 @@ const AnswerContent = styled.div`
   font-size: 1.1rem;
 `;
 
+const NoAnswerContent = styled.div`
+  display: flex;
+  width: 100%;
+  font-size: 1.1rem;
+  color: var(--Red-50);
+`;
+
 const HrStyled = styled.hr`
   width: 100%;
   margin: 8px 0;
@@ -87,11 +98,22 @@ const HrStyled = styled.hr`
 export function FeedCard(props) {
   const { answer, content, question } = props;
   const { createdAt, imageSource, name } = question;
+
+  let answerText, color;
+  if (answer === null) {
+    answerText = "미답변";
+    color = "unanswered";
+  } else if (answer.isRejected) {
+    answerText = "답변 거절";
+    color = "rejected";
+  } else {
+    answerText = "답변 완료";
+    color = "answered";
+  }
+
   return (
     <FeedCardStyled className="shadow-1pt">
-      <AnsweredStyled answered={answer}>
-        {answer ? "답변완료" : "미답변"}
-      </AnsweredStyled>
+      <AnsweredStyled color={color}>{answerText}</AnsweredStyled>
       <QuestionContainer>
         <DateStyled>질문 · {getTimeAgo(createdAt)}</DateStyled>
         {content}
@@ -102,7 +124,11 @@ export function FeedCard(props) {
           <AnswerStyled>
             {name}
             <DateStyled>{getTimeAgo(answer.createdAt)}</DateStyled>
-            <AnswerContent>{answer.content}</AnswerContent>
+            {answer.isRejected ? (
+              <NoAnswerContent>답변 거절</NoAnswerContent>
+            ) : (
+              <AnswerContent>{answer.content}</AnswerContent>
+            )}
           </AnswerStyled>
         </AnswerWrapper>
       )}
