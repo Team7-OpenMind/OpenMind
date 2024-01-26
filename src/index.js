31b9c42577043487f6015475e228dc0e715082fd
ohddang <ohddang509@gmail.com>
2024-01-26 15:44:00 +0900

    OM-13 chore: webpack 설정

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import App from "App";
-import "common.css";
+import App from "./App.jsx";
+import "./common.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 
@@ -16,9 +16,9 @@ const queryClient = new QueryClient({
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
-  </React.StrictMode>,
+  // <React.StrictMode>
+  <QueryClientProvider client={queryClient}>
+    <App />
+  </QueryClientProvider>,
+  // </React.StrictMode>,
 );
