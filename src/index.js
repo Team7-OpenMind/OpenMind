acc1e0b90ee18e41656257dbd3282456d430d335
ohddang <ohddang509@gmail.com>
2024-01-26 15:53:35 +0900

    OM-13 fix: webpack 설정 변경으로 index.js 파일에서 import 'App.jsx' -> 'App' 으로 변경

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import App from "./App.jsx";
+import App from "./App";
 import "./common.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
