03ff29be86127218a35aa6908ab78ff9bfd7930c
Jinah <monsatfri@gmail.com>
2024-01-27 00:10:08 +0900

    OM-24 feat: Redux reducer에 subjectSlice 추가

--- src/store/index.js
+++ src/store/index.js
@@ -1,20 +1,21 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
-import storage from "redux-persist/lib/storage";
-import userSlice from "./userSlice";
 import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import questionSlice from "./questionSlice";
+import subjectSlice from "./subjectSlice";
 
 const reducers = combineReducers({
   // 여러 reducer를 하나로 합쳐주는 역할
-  user: userSlice.reducer,
+  subject: subjectSlice.reducer,
 });
 
 const persistedConfig = {
   key: "root", // reducer key
   storage, // local storage에 저장
-  whitelist: ["user"], // user reducer만 local storage에 저장
+  whitelist: ["question", "subject"], // user reducer만 local storage에 저장
 };
 
-const persistedReducer = persistReducer(persistedConfig, reducers); // reducer를 반환시키는 API
+const persistedReducer = persistReducer(persistedConfig, reducers); // persistReducer를 이용하여 reducer에 대한 정보를 저장, reducer를 반환시키는 API
 
 const store = configureStore({
   reducer: persistedReducer,
