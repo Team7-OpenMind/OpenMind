00726bd575ef880b2ea0b40a5280078cfb4d24a4
Jinah <monsatfri@gmail.com>
2024-01-27 15:48:42 +0900

    OM-24 fix: 사용하지 않는 import 제거

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,6 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
-import questionSlice from "./questionSlice";
 import subjectSlice from "./subjectSlice";
 
 const reducers = combineReducers({
