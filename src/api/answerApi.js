e1b579eb6721f5afacddf7e727facf25721878b1
Jinah <monsatfri@gmail.com>
2024-02-02 18:13:10 +0900

    OM-66 refactor: api 함수 내 코드를 간단하게 축약

--- src/api/answerApi.js
+++ src/api/answerApi.js
@@ -1,6 +1,14 @@
 import axios from "axios";
 
-// 질문 생성하면 questionId가 생김 이거 받아와서 쓰면 된다
+/**
+ * 답변을 생성하는 함수
+ * @param {object} answerData 생성할 답변의 데이터를 포함하는 객체
+ * @param {string} answerData.questionId 답변이 속한 질문의 ID
+ * @param {string} answerData.content 생성할 답변의 내용
+ * @param {boolean} answerData.isRejected 답변의 거절 상태 (true: 거절됨, false: 거절되지 않음)
+ * @param {string} answerData.team 답변을 생성하는 팀의 ID 또는 이름
+ * @returns {Promise} Axios 요청의 응답 데이터를 포함하는 프로미스 객체
+ */
 export const createAnswer = async ({
   questionId,
   content,
@@ -17,25 +25,29 @@ export const createAnswer = async ({
   return data;
 };
 
-export const getAnswer = async (answerId) => {
-  const res = await axios.get(`/answers/${answerId}/`);
-  const data = res;
-  return data;
-};
+/**
+ * 특정 답변을 가져오는 함수
+ * @param {string} answerId 가져올 답변의 ID
+ * @returns {Promise} Axios 요청의 응답 데이터를 포함하는 프로미스 객체
+ */
+export const getAnswer = async (answerId) =>
+  await axios.get(`/answers/${answerId}/`);
 
-// 답변 수정하는 기능
-export const putUpdateAnswer = async (answerId, { content, isRejected }) => {
-  const res = await axios.put(`/answers/${answerId}/`, {
-    content,
-    isRejected,
-  });
-  const data = res;
-  return data;
-};
+/**
+ * 답변을 수정하는 함수
+ * @param {string} answerId 수정할 답변의 ID
+ * @param {object} data 수정할 내용과 상태를 포함하는 객체
+ * @param {string} data.content 수정할 답변의 내용
+ * @param {boolean} data.isRejected 수정할 답변의 거절 상태 (true: 거절됨, false: 거절되지 않음)
+ * @returns {Promise} Axios 요청의 응답 데이터를 포함하는 프로미스 객체
+ */
+export const putUpdateAnswer = async (answerId, { content, isRejected }) =>
+  await axios.put(`/answers/${answerId}/`, { content, isRejected });
 
-// 질문 삭제하는 기능
-export const deleteQuestion = async (questionId) => {
-  const res = await axios.delete(`/questions/${questionId}/`);
-  const data = res;
-  return data;
-};
+/**
+ * 질문을 삭제하는 함수
+ * @param {string} questionId 삭제할 질문의 ID
+ * @returns {Promise} Axios 요청의 응답 데이터를 포함하는 프로미스 객체
+ */
+export const deleteQuestion = async (questionId) =>
+  await axios.delete(`/questions/${questionId}/`);
--- src/api/reactionApi.js
+++ src/api/reactionApi.js
@@ -1,13 +1,10 @@
 import axios from "axios";
 
 //질문 가져오는 api
-export const getQuestion = async ({ questionId }) => {
-  const res = await axios.get(`/questions/${questionId}/`);
-  const data = res.data;
-  return data;
-};
+export const getQuestion = async ({ questionId }) =>
+  (await axios.get(`/questions/${questionId}/`)).data;
 
-//리약션 카운트 하는 api
+//리액션 카운트 하는 api
 export const countReaction = async (questionId, reaction) => {
   const res = await axios.post(
     `/questions/${questionId}/reaction/`,
